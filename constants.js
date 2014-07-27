b8a4e5de10c475a2a9561453a40762f4b3324776
Gus <gus@hogg-blake.co.uk>
2014-07-27 12:10:15 +0100

    adds REMATCH_TIMEOUT constant and renames CHALLENGE_TIMEOUT to SEEK_TIMEOUT

--- constants.js
+++ constants.js
@@ -1,6 +1,7 @@
 define(function(require) {
 	return {
-		CHALLENGE_TIMEOUT: 1000 * 60,
+		SEEK_TIMEOUT: 1000 * 60,
+		REMATCH_TIMEOUT: 1000 * 15,
 		TIME_FOR_MOVES_BEFORE_CLOCK_START: 1000 * 30
 	};
 });
